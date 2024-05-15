a1a9cda83a07eb7f15c714df0338e751c601df00
Venkat.G <venki.g712@gmail.com>
2024-05-15 23:37:53 +1000

    post call chnages added

---
+++ src/test/java/restAPIGorest/PostCallAPIGorest.java
@@ -0,0 +1,56 @@
+package restAPIGorest;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import goRestAPIPojo.UserPayload;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class PostCallAPIGorest {
+	
+@DataProvider(name = "userdata")	
+ public Object[][] userCreation() {
+	 
+	return new Object[][] {{"Rajesh","rajesh@email.com","male","Active"},
+		{"Raju","raju@email.com","male","Active"},
+		{"Rajaram","rajaram@email.com","male","Active"}
+	};
+	 
+ }
+	
+	
+ @Epic("GoRest API Post call features")	
+ @Story("GoRest API Post call verification")
+ @Description("Veriy the post call of GoRest API")	
+ @Test(dataProvider = "userdata")	
+ public void PostCallGorestAPI(String name,String email,String gender,String status) {
+	 
+	 RestAssured.baseURI= "https://gorest.co.in";
+		RestAssured.basePath="/public/v2/users";
+		
+		
+		
+		UserPayload user = new UserPayload(name,email,gender,status);
+		
+		given().log().all()
+		.contentType(ContentType.JSON)
+		.body(user)
+		.auth()
+		.oauth2("f044dec310ab566e156bb8acb2c54d7b0bde89340640166169ad8846215f54cf")
+		.when().log().all()
+		.post()
+		.then().log().all()
+		.statusCode(201);
+	 
+	 
+ }	
+	
+
+}
