428f637c8627399955fe497f362d48dc92b5e2af
Venkat.G <venki.g712@gmail.com>
2024-05-15 22:23:11 +1000

    get call api  severity and user story added

--- src/test/java/restAPIGorest/GetCallAPIGorest.java
+++ src/test/java/restAPIGorest/GetCallAPIGorest.java
@@ -5,12 +5,21 @@ import static org.hamcrest.Matchers.*;
 
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 
 public class GetCallAPIGorest {
 	
+	@Epic("EPIC -100: Go Rest API - Get call features")
+	@Story("US - 101: API http method Get call verification")
+	@Description("GorestAPI Get call verification") 
+	@Severity(SeverityLevel.MINOR)	
 	@Test
 	public void getcallAPI() {
 		
